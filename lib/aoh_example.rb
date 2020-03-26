dc2ea2a4a1163a7d4bb3e2712ff905788934bfff
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-26 22:25:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,11 +11,15 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   
   halfway_array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  return halfway_array
 end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  new_array = [{ name:  "Don Gately", occupation: "Live-in Staff" }, { name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" }, { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+  return new_array
+  
 end
 
 def aoh_lookup(aoh, row, key)
